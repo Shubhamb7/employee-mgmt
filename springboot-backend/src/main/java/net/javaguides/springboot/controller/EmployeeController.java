de526d03af2fa332fd0d730d8c01973841075a44
Shubhamb7 <sbelgaonkar@AD.NSEIT.COM>
2022-01-25 18:14:19 +0530

    changes in application files

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -27,29 +29,41 @@ public class EmployeeController {
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
-	
+
+	Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
 	// get all employees
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.findAll();
-	}		
-	
+	}
+
 	// create employee rest api
 	@PostMapping("/employees")
 	public Employee createEmployee(@RequestBody Employee employee) {
-		return employeeRepository.save(employee);
+		Employee tempEmployee = employeeRepository.save(employee);
+		logger.info("user created with ID: {}", employee.getId());
+		return tempEmployee;
 	}
-	
+
 	// get employee by id rest api
 	@GetMapping("/employees/{id}")
 	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
 		Employee employee = employeeRepository.findById(id)
+				.orElse(null);
+
+		if(employee!=null){
+			logger.info("user found: {}", employee);
+			return ResponseEntity.ok(employee);
+		}else{
+			logger.error("user not found with ID: {}", id);
+			Employee employee2 = employeeRepository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
-		return ResponseEntity.ok(employee);
+			return ResponseEntity.ok(employee2);
+		}
 	}
-	
+
 	// update employee rest api
-	
 	@PutMapping("/employees/{id}")
 	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
 		Employee employee = employeeRepository.findById(id)
@@ -66,14 +80,23 @@ public class EmployeeController {
 	// delete employee rest api
 	@DeleteMapping("/employees/{id}")
 	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
-		Employee employee = employeeRepository.findById(id)
+		Employee employee = employeeRepository.findById(id).orElse(null);
+
+		if(employee!=null){
+			logger.warn("user deleted: {}", id);
+			employeeRepository.delete(employee);
+			Map<String, Boolean> response = new HashMap<>();
+			response.put("deleted", Boolean.TRUE);
+			return ResponseEntity.ok(response);
+		}else{
+			logger.error("user to be deleted not found with ID: {}", id);
+			Employee employee2 = employeeRepository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
-		
-		employeeRepository.delete(employee);
-		Map<String, Boolean> response = new HashMap<>();
-		response.put("deleted", Boolean.TRUE);
-		return ResponseEntity.ok(response);
+				employeeRepository.delete(employee2);
+				Map<String, Boolean> response = new HashMap<>();
+				response.put("deleted", Boolean.TRUE);
+				return ResponseEntity.ok(response);
+		}
+
 	}
-	
-	
 }
