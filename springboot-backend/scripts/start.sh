8178eb1cafcbdfb2fd010c1706d9bdc940b99dc6
Shubham Belgaonkar <shubhamb756@gmail.com>
2023-01-10 11:20:12 +0530

    Update start.sh

--- springboot-backend/scripts/start.sh
+++ springboot-backend/scripts/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cd /home/ec2-user/server
+cd /home/ubuntu/employee-mgmt/springboot-backend
 sudo /usr/bin/java -jar -Dserver.port=80 \
   *.jar > /dev/null 2> /dev/null < /dev/null &
