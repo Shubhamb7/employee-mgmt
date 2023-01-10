011af19eab2a1bc224535cab02e19d822157f439
Shubham Belgaonkar <shubhamb756@gmail.com>
2023-01-10 11:36:00 +0530

    Update start.sh

--- springboot-backend/scripts/start.sh
+++ springboot-backend/scripts/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cd /home/ubuntu/employee-mgmt/springboot-backend/target
+cd /var/lib/jenkins/workspace/deployment\ job/springboot-backend/target
 sudo /usr/bin/java -jar -Dserver.port=80 \
   *.jar > /dev/null 2> /dev/null < /dev/null &
